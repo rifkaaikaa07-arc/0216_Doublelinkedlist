c5fff5d76dcab8a0fd049c3c050590208966a41d
rifkaaikaa07-arc <rifkaaikaa07@gmail.com>
2026-04-28 13:05:33 +0700

    menambahakn step 6-9 untuk melengkapi void addnode

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -47,7 +47,38 @@ class DoubleLinkedList
             //step 5: START.prev = newNode (if START exists)
             if (START != NULL)
                 START->prev = newNode;
+
+            //step 6: newNode.prev = NULL 
+            newNode->prev = NULL;
+
+            //step 7: START = newNode
+            START = newNode;
+            return;
         }
+
+        // insert in between node
+        // step 8: locate position for insertion
+        Node *current = START;
+        while (current->next != NULL && current->next->noMhs < nim)
+        {
+            current = current->next;
+        }
+
+        if (current->next != NULL && nim == current->next->noMhs)
+        {
+            cout << "\nDuplicate roll numbers not allowed" << endl;
+            return;
+        }
+
+        // step 9: insert between current and current->next
+        newNode->next = current->next;  // step 9a: newNode.next = current.next
+        newNode->prev = current;       // step 9b: newNode.prev = current
+
+        //insert last node
+        if (current->next != NULL)
+            current->next->prev = newNode;  // step 9c: current.next.prev = newNode
+        
+            current->next = newNode;  // sep 9d: current.next = newNode
 
     }
 }; 
