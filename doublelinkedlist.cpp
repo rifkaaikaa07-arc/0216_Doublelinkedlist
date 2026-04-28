f282a4101864472fd23f8416616b33828ce3a29e
rifkaaikaa07-arc <rifkaaikaa07@gmail.com>
2026-04-28 13:04:20 +0700

    menambahkan step 1-5 pada void addnode

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -23,6 +23,31 @@ class DoubleLinkedList
 
     void addNode()
     {
+        int nim;
+        cout << "\nEnter the roll number of the student: ";
+        cin >> nim;
+
+        // step 1: allocate memory for new node
+        Node *newNode = new Node();
+
+        //step 2: assign value to the data fields
+        newNode->noMhs = nim;
+
+        //step 3: insert at beginning if list is empty or nim is smallest
+        if (START == NULL || nim <= START->noMhs)
+        {
+            if (START != NULL && nim == START->noMhs)
+            {
+                cout << "\nDuplicate number not allowed" << endl;
+                return;
+            }
+            //stsep 4: newNode.next = START
+            newNode->next = START;
+
+            //step 5: START.prev = newNode (if START exists)
+            if (START != NULL)
+                START->prev = newNode;
+        }
 
     }
-};
+}; 
