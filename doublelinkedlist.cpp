0218eb8f3efffcecd3d9f08e1a215a5b54252e2c
rifkaaikaa07-arc <rifkaaikaa07@gmail.com>
2026-04-28 13:07:45 +0700

    menambahkan step 1-4 pada void hapus

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -84,6 +84,44 @@ class DoubleLinkedList
 
     void hapus()
     {
-        
+        if (START ==NULL)
+        {
+            cout << "\nList is empty" << endl;
+            return;
+        }
+
+        cout << "\nEnter the roll number of the student whose is to be deleted: ";
+        int rollNo;
+        cin>> rollNo;
+
+        Node *current = START;
+
+        // step 1: traverse the list to find the node
+        while (current != NULL && current->noMhs != rollNo)
+            current = current->next;
+
+        if (current == NULL)
+        {
+            cout << "Record not found" << endl;
+            return;
+        }
+
+        // step 2: if node is at the beginning
+        if (current == START)
+        {
+            START = current->next; // step 2a: START = START.next
+            if (START != NULL)
+                START->prev = NULL; // step 2b: START.prev = NULL
+            
+        }
+        else
+        {
+            // step 3: Link previous node to next of current
+            current->prev->next = current->next;
+
+            // step 4: if current is not the last node
+            if (current->next != NULL)
+                current->next->prev = current->prev;
+        }
     }
 }; 
