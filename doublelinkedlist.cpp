0860845b380ae1c40ea68d929f0e39ce10561f9e
rifkaaikaa07-arc <rifkaaikaa07@gmail.com>
2026-04-28 13:10:19 +0700

    menambahkan step 1-3 untuk void traverse

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -131,6 +131,26 @@ class DoubleLinkedList
 
     void traverse()
     {
+        if (START == NULL)
+        {
+            cout << "\nList is empty" << endl;
+            return;
+        }
 
+        // step 1: mark first node as currentNode
+        Node *currentNode = START;
+
+        // step 2: repeat until currentNode == NULL
+        cout << "\nRecords in ascending order of roll number are:\n";
+        int i = 0;
+        while (currentNode != NULL)
+        {
+    
+        cout << i + 1 << "." << currentNode->noMhs << " " << endl;
+
+        // step 3: move to previous node
+        currentNode = currentNode->prev;
+        i--;
+        }
     }
 }; 
